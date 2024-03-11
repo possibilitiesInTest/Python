6c5403d398ed40b62fdb639882398ed6393f2f79
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-11 19:11:43 -0400

    compile emails in dictionary:
         set line_list to empty arr
         for line in input:
              set columns to line split by pipe
              access email address as lookup_val t col zero
    
              if d.get(email_key) is None, not in dict
    
                   get last val in dict and increment by one
                   set email_key in dict with next_num as val
                   set col at zero to str of next_num
                   line_list.append cols joined by pipe
    
              else
                   set cols at zero to d.get(email_key)
                   append cols joined by pipe to line_list
    
          set fr to line_list

---
+++ src/Ahmad-Imtiaz-Developer-Course/HelloWorld/venv/Section_04/assignment_09.py
@@ -0,0 +1,156 @@
+# Assignment 9
+
+"""
+We have 2 variables. fr and d. fr is a list of strings and d is a dictionary with email
+addresses as keys and numbers as values (numbers in string format).
+Write code to replace the email address in each of the strings in the fr list with
+the associated value of that email looked up from the dictionary d.
+If the dictionary does not contain the email found in the list, add a new entry
+in the dictionary for the email found in the fr list. The value for this new email key
+will be the next highest value number in the dictionary in string format.
+
+Once the dictionary is populated with this new email key and a new number value,
+replace that email's occurrence in the fr list with the number value.
+
+The output of running your completed code should be the following:
+
+Value of fr:
+['199|4|11|GDSPV', '199|16|82|GDSPV', '205|12|82|GDSPV', '206|19|82|GDSPV']
+Value of d:
+{'7@comp1.COM': '199', '8@comp4.COM': '200', '13@comp1.COM': '205', '26@comp1.COM': '206'}
+
+"""
+
+# Don't manually change fr and d.
+fr = [
+'7@comp1.COM|4|11|GDSPV',
+'7@comp1.COM|16|82|GDSPV',
+'13@comp1.COM|12|82|GDSPV',
+'26@comp1.COM|19|82|GDSPV'
+]
+
+d= {
+'7@comp1.COM': '199',
+'8@comp4.COM': '200',
+'13@comp1.COM': '205'
+}
+
+
+# Your Code Below:
+# --------------------------------------
+line_list = []
+for line in fr:
+    # split input by pipe
+    # access email address as first val
+    columns = line.split("|")
+<<<<<<< HEAD
+    # print(columns)
+=======
+>>>>>>> ebee6a3ba295552ac0f574b6440213fc3f0536c8
+    lookup_val = columns[0]
+    # if lookup_val not in d.keys()
+    if(d.get(lookup_val) is None): # if lookup_val not in dict
+
+        # get last val in dict and increment by one
+        # set lookup_Val in dict with next num as val
+
+        next_number = max([int(n) for n  in d.values()]) + 1
+        d[lookup_val] = str(next_number)
+<<<<<<< HEAD
+
+        # set columns at first index in line_list to next_num
+        # append cols joined by pipe
+=======
+>>>>>>> ebee6a3ba295552ac0f574b6440213fc3f0536c8
+        columns[0] = str(next_number)
+        line_list.append("|".join(columns))
+
+        # email found existing in dict
+    else:
+<<<<<<< HEAD
+        # set cols at zero to lookup_val
+        # append cols joined by pipe to line_list
+        columns[0] = d.get(lookup_val)
+        line_list.append("|".join(columns))
+
+    fr = line_list
+
+=======
+        columns[0] = d.get(lookup_val)
+        line_list.append("|".join(columns))
+
+>>>>>>> ebee6a3ba295552ac0f574b6440213fc3f0536c8
+# don't change the lines below:
+# --------------------------------------
+print("Value of fr: ")
+print(fr)
+print("Value of d:")
+print(d)
+<<<<<<< HEAD
+# print('line_list: ', line_list)
+# values = ['200', '21', '190']
+=======
+print('line_list: ', line_list)
+
+
+
+# values= ['200', '21', '190']
+>>>>>>> ebee6a3ba295552ac0f574b6440213fc3f0536c8
+# print(max([int(n) for n in values]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution:
+#
+# line_list = []
+# # this won't work for missing keys
+# for line in fr:
+#     columns = line.split("|")
+#     lookup_val = columns[0]
+#
+#     # if lookup_val not in d.keys():
+#     if(d.get(lookup_val) is None): # Can't find in dict
+#
+#         # get the next number + 1 from dict
+#         next_number = int(max(d.values())) + 1
+#         d[lookup_val] = str(next_number)
+#         columns[0] = str(next_number)
+#         line_list.append("|".join(columns))
+#     else:
+#         columns[0] = d.get(columns[0])
+#         line_list.append("|".join(columns))
+#
+# fr = line_list
+#
